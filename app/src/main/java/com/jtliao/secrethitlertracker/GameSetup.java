73d36b89c0a5d2fa6794ba239124d6f777df4eb8
jtliao <jtliao9@yahoo.com>
2016-12-28 22:19:27 -0800

    added error messages

--- app/src/main/java/com/jtliao/secrethitlertracker/GameSetup.java
+++ app/src/main/java/com/jtliao/secrethitlertracker/GameSetup.java
@@ -1,17 +1,40 @@
 package com.jtliao.secrethitlertracker;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.app.AlertDialog;
 
 public class GameSetup extends AppCompatActivity {
     int numPlayers = 5;
+    AlertDialog.Builder decrementBuilder;
+    AlertDialog.Builder incrementBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setup);
+
+        decrementBuilder = new AlertDialog.Builder(this);
+        decrementBuilder.setMessage("Cannot have less than 5 players!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do things
+                    }
+                });
+
+        incrementBuilder = new AlertDialog.Builder(this);
+        incrementBuilder.setMessage("Cannot have more than 10 players!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do things
+                    }
+                });
     }
 
     public void startGame(View view) {
@@ -22,7 +45,8 @@ public class GameSetup extends AppCompatActivity {
 
     public void increment(View view) {
         if (numPlayers >= 10) {
-
+            AlertDialog alert = incrementBuilder.create();
+            alert.show();
         }
         else {
             numPlayers++;
@@ -32,7 +56,8 @@ public class GameSetup extends AppCompatActivity {
 
     public void decrement(View view) {
         if (numPlayers <= 5) {
-
+            AlertDialog alert = decrementBuilder.create();
+            alert.show();
         }
         else {
             numPlayers--;
